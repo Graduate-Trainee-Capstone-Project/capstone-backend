86f56a4b4e1470ac39b8bad2572eabfe6c403af3
aolurombi <aolurombi@qore.inc>
2026-09-15 13:20:53 +0100

    removal of health and waether controller

--- OnboardingPlatform.Data/Migrations/20260915120043_AddAshisTableField.Designer.cs
+++ OnboardingPlatform.Data/Migrations/20260915120043_AddAshisTableField.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnboardingPlatform.Data.Implementations;
 
@@ -11,9 +12,11 @@ using OnboardingPlatform.Data.Implementations;
 namespace OnboardingPlatform.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260915120043_AddAshisTableField")]
+    partial class AddAshisTableField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
