1444544374408131ca4bc5a5b84c5d9ac21ec549
ashijunior88 <emmanuelashibuogwu12@gmail.com>
2026-09-16 13:51:54 +0100

    Update utilities

---
+++ OnboardingPlatform.Utilities/JsonElementExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OnboardingPlatform.Utilities
+{
+    public static class JsonElementExtensions
+    {
+        public static object? ToObject(this JsonElement element)
+        {
+            switch (element.ValueKind)
+            {
+                case JsonValueKind.Object:
+                    var obj = new Dictionary<string, object?>();
+                    foreach (var prop in element.EnumerateObject())
+                        obj[prop.Name] = prop.Value.ToObject();
+                    return obj;
+
+                case JsonValueKind.Array:
+                    var list = new List<object?>();
+                    foreach (var item in element.EnumerateArray())
+                        list.Add(item.ToObject());
+                    return list;
+
+                case JsonValueKind.String:
+                    return element.GetString();
+
+                case JsonValueKind.Number:
+                    if (element.TryGetInt64(out var l)) return l;
+                    if (element.TryGetDecimal(out var dec)) return dec;
+                    return element.GetDouble();
+
+                case JsonValueKind.True:
+                    return true;
+
+                case JsonValueKind.False:
+                    return false;
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
